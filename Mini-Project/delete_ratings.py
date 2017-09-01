360f05716134b4cfcec7c41c480309559688ba8b
Saumya Shah <shahsaumya0108@gmail.com>
2017-09-01 16:07:09 +0530

    Changes

--- Mini-Project/delete_ratings.py
+++
@@ -1,7 +0,0 @@
-from recombee_api_client.api_client import RecombeeClient
-from recombee_api_client.api_requests import *
-client = RecombeeClient('sns', 'cqLZqZnboKlyVKS7EhhYYyM8BflGRDlizngZbljA3kp67tjd1FKfH3WaXLNSXl7F')
-count = 1
-users=[]
-for i in range(1,672):
-    client =
--- Mini-Project/recommend.py
+++ Mini-Project/recommend.py
@@ -1,6 +1,7 @@
 from recombee_api_client.api_client import RecombeeClient
 from recombee_api_client.api_requests import *
 client = RecombeeClient('sns', 'cqLZqZnboKlyVKS7EhhYYyM8BflGRDlizngZbljA3kp67tjd1FKfH3WaXLNSXl7F')
-
-recommended = client.send(UserBasedRecommendation('672',5))
+print(client.send(GetItemValues('1')))
+print(client.send(ListItemProperties()))
+recommended = client.send(UserBasedRecommendation('672',5,filter="Animation" in 'genre'))
 print(recommended)
