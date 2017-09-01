107e724fa482914a936edcd0028679d382589dae
Saumya Shah <shahsaumya0108@gmail.com>
2017-09-01 16:03:44 +0530

    Add ratings

---
+++ Mini-Project/add_ratings.py
@@ -0,0 +1,25 @@
+from recombee_api_client.api_client import RecombeeClient
+from recombee_api_client.api_requests import *
+import csv
+client = RecombeeClient('sns', 'cqLZqZnboKlyVKS7EhhYYyM8BflGRDlizngZbljA3kp67tjd1FKfH3WaXLNSXl7F')
+ratings=[]
+with open('ml-latest-small/sample_user.csv') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        user_id = row[0]
+        movie_id = row[1]
+        numStars = float(row[2])
+        rating = (numStars-3)/2.0
+        interaction = AddRating(user_id, movie_id,rating,#optional parameters:
+                cascade_create=True
+                )
+        ratings.append(interaction)
+
+
+try:
+    print('sending')
+    client.send(Batch(ratings))
+    print('Send')
+
+except APIException as e:
+    print(e)
