9223e4d90d88475bb3a64c17e1e8f43ee3f728e5
Saumya Shah <shahsaumya0108@gmail.com>
2017-09-15 22:05:11 +0530

    Prime Numbers using PHP

--- Mini-Project/add_db.py
+++ Mini-Project/add_db.py
@@ -2,9 +2,10 @@ from recombee_api_client.api_client import RecombeeClient
 from recombee_api_client.api_requests import *
 import csv
 from datetime import date
-client = RecombeeClient('sns', 'cqLZqZnboKlyVKS7EhhYYyM8BflGRDlizngZbljA3kp67tjd1FKfH3WaXLNSXl7F')
-#client.send(ResetDatabase())
-client.send(AddItemProperty('title','string'))
-client.send(AddItemProperty('genre','set'))
-client.send(AddItemProperty('imdbId','string'))
-client.send(AddItemProperty('tmdbId','string'))
+client = RecombeeClient(
+    'sns', 'cqLZqZnboKlyVKS7EhhYYyM8BflGRDlizngZbljA3kp67tjd1FKfH3WaXLNSXl7F')
+# client.send(ResetDatabase())
+client.send(AddItemProperty('title', 'string'))
+client.send(AddItemProperty('genre', 'set'))
+client.send(AddItemProperty('imdbId', 'string'))
+client.send(AddItemProperty('tmdbId', 'string'))
