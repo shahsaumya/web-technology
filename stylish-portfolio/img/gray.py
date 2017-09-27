7e6468d3e1b73ee7f6fbb2c1e23099720c82baa6
Saumya Shah <shahsaumya0108@gmail.com>
2017-09-27 19:40:17 +0530

    Removed unwanted files

--- stylish-portfolio/img/gray.py
+++
@@ -1,8 +0,0 @@
-from numpy import *
-import cv2
-mazeImg = cv2.imread('film.jpg')
-grayImg = cv2.cvtColor(mazeImg, cv2.COLOR_BGR2GRAY)
-cv2.imwrite( "film.jpg", grayImg );
-cv2.imshow("final",grayImg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
